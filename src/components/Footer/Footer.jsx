a073d92052ed684b32dd458baeb37e7fbbc88d7c
RowanKinross <rowankinross@yahoo.co.uk>
2024-02-26 22:26:16 +0000

    commented_out_images_to_test_if_thats_the_issue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import CV from '/Rowan_Kinross_Web_Developer_CV_2023.pdf'
+// import CV from '/Rowan_Kinross_Web_Developer_CV_2023.pdf'
 import "./footer.css"
 
 function Footer() {
@@ -17,7 +17,7 @@ function Footer() {
         <h6 className="email">rowan.kinross@outlook.com</h6>
       </a> 
 
-      <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href={CV} target="_blank">CV</a>
+      {/* <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href={CV} target="_blank">CV</a> */}
     </footer>
     </div>
   )
